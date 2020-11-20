f36f50a632b5602669bea5d481b75731137640e7
微风的龙骑士 风游迩 <dk_breeze@qq.com>
2020-11-20 18:30:47 +0800

    更新项目文档

--- stellaris-modding-support/src/test/kotlin/CollectStellarisPath.kt
+++ stellaris-modding-support/src/test/kotlin/CollectStellarisPath.kt
@@ -1,7 +1,32 @@
+import java.io.*
+
 fun main() {
-	collectStellarisPath("stellaris-modding-support\\src\\test\\resources\\stdlib")
+	val rootPath = "stellaris-modding-support\\src\\test\\resources\\stdlib"
+	val propsPath = "stellaris-modding-support\\src\\test\\resources\\types.properties"
+	val props = readFromProps(propsPath)
+	val paths = collectStellarisPath(rootPath)
+	writeToProps(propsPath,paths,props)
+	
 }
 
 private fun collectStellarisPath(path:String):List<String>{
-	return listOf()
+	val rootFile = File(path)
+	val rootPath = rootFile.path
+	return rootFile.walk().mapNotNull { file->
+		if(!file.isDirectory) return@mapNotNull null
+		val p = file.path.removePrefix(rootPath).replace("\\","/")
+		if(p.isBlank()) null else p
+	}.toList()
+}
+
+private fun readFromProps(path:String): Map<String, String> {
+	return File(path).readText().lines().map {
+		val kv = it.split('=')
+		kv[0].trim() to kv[1]
+	}.toMap()
+}
+
+private fun writeToProps(path:String,paths:List<String>,props:Map<String,String>){
+	val text = paths.joinToString("\n"){ "$it = ${props[it].let{ s -> if(s == null || s.isBlank()) it else s}}" }
+	File(path).writeText(text)
 }
